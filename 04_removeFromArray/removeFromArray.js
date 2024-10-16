19fb7a8a927a2687524558ceaf1258184ebd9fd5
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2024-10-16 21:59:40 +0300

    feat(js): remove specified items from array using loops and splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function(array, ...itemsToRemove) {
+    // Loop through each item in the itemsToRemove array
+    for (const item of itemsToRemove) {
+        let index; // Declare a variable to hold the index of the current item
+        
+        // Continue looping as long as the item is found in the array
+        while ((index = array.indexOf(item)) !== -1) {
+            // Remove the item at the found index
+            array.splice(index, 1);
+        }
+    }
 
+    // Return the modified array after all specified items have been removed
+    return array;
 };
 
 // Do not edit below this line
