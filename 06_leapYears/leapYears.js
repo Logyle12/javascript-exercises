c2a648cc7e012b739ba506f3892660ac1b137967
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2024-10-17 00:27:21 +0300

    feat(js): implement logic to check if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
 const leapYears = function(leapYear) {
 
+// Function to determine if a given year is a leap year
+const isLeapYear = year => 
+    // A year is a leap year if it is divisible by 4
+    year % 4 == 0 && 
+    // But not if it is divisible by 100, unless it is also divisible by 400
+    year % 100 != 0 || 
+    year % 400 == 0; 
 
 
 
