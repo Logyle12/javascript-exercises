b134c363a1905d423833a51e6e7fd70a19b2907e
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2024-10-16 19:35:24 +0300

    feat(js): add error handling for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
     let repeatedString = '';
 
     for (let index = 0; index < num; index++) {
