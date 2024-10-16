aae0cc89449c3080c11425849b8f6e5ba76b245d
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2024-10-17 01:17:12 +0300

    feat(js): implement conversion logic for Celsius and Fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
 const convertToCelsius = function(temperature) {
+  temperature = (temperature - 32) * (5/9);
+  return roundToDecimalPlaces(temperature, 1);
   
 };
 
 const convertToFahrenheit = function(temperature) {
+  temperature = (temperature * (9/5)) + 32;
+  return roundToDecimalPlaces(temperature, 1);
 };
+
 
 // Do not edit below this line
 module.exports = {
