2f50a01c10cb4c10be2f9bfbb143bb5f514b9160
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2024-10-16 22:49:14 +0300

    feat(js): add helper function to validate non-negative integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,6 @@
 const sumAll = function(m, n) {
-
+    // Helper function to validate that the number is a non-negative integer
+    const isValid = num => num >= 0 && Number.isInteger(num);
 
 
 };
