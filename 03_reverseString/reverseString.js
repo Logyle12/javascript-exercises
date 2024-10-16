dd345e9e132fb0602bd88654f9fca0e2df270c04
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2024-10-16 20:02:16 +0300

    feat(js): iterate and concatenate characters to construct reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,11 @@ const reverseString = function(string) {
     let reverseString = '';
     const stringArray = string.split("");
 
+    for (let index = 0; index < string.length; index++) {
+        reverseString += stringArray.pop(); 
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
