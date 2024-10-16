1f7d9d71e55abf80bf22ca4d139e0afd9c0fc312
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2024-10-17 01:18:39 +0300

    feat(js): add function to round numbers to specified decimal places

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -9,7 +9,10 @@ const convertToFahrenheit = function(temperature) {
   return roundToDecimalPlaces(temperature, 1);
 };
 
-
+const roundToDecimalPlaces = (num, decimalPlaces) => {
+  const factor = Math.pow(10, decimalPlaces); // Calculate 10 to the power of the number of decimal places
+  return Math.round(num * factor) / factor; // Round and divide back
+};
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
