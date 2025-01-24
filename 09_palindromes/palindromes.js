30205d4ec17ab3ab2d07552cdb9f82a818de9d38
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2025-01-24 20:30:14 +0300

    feat(js): compare tokens to validate palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,6 +2,7 @@ const palindromes = function (text) {
     const textTokens = text.toLowerCase().split(/\W|()/g);
     const cleanedText = textTokens.join('');
     const reversedText = textTokens.reverse().join('');
+    return cleanedText === reversedText;
 };
 
 // Do not edit below this line
