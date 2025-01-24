cfda4a6544f2582f31202139c9e5cc361a67f56c
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2025-01-24 21:02:51 +0300

    feat: add loop with early exit for first two Fibonacci numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
 const fibonacci = function(n) {
     const num = parseInt(n);
     const sequence = [1, 1];
+    for (let i = 1; i <= num; i++) {
+        if (num  <=2) {
+            break;
+        }
+    }
 
 };
 
