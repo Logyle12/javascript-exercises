9e11d6c2f82a5334c9322a03f5cbbdd4a3080f63
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2025-01-24 20:29:50 +0300

    feat(js): generate reversed text for palindrome comparison

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
 const palindromes = function (text) {
     const textTokens = text.toLowerCase().split(/\W|()/g);
     const cleanedText = textTokens.join('');
+    const reversedText = textTokens.reverse().join('');
 };
 
 // Do not edit below this line
